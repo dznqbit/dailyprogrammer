89d48eee01aba7286d93e8b61de4b3ae28d7b50a
Quinton Harris (dznqbit) <quinton.harris@gmail.com>
2013-08-29 04:32:29 -0700

    Built problem

--- arithmetic/arithmetic.py
+++ arithmetic/arithmetic.py
@@ -7,5 +7,17 @@ class app():
 
   def run(self):
     bounds  = [int(x) for x in string.split(raw_input(), " ")]
+    self.question(bounds)
+
+  def question(self, bounds):
     numbers = [random.randrange(bounds[0], bounds[1]) for i in range(4)]
-    print(str(numbers).strip('[]'))
+
+    equation = reduce(
+      lambda memo, num: 
+        ' '.join([memo, random.choice(["+", "-", "x"]), str(num)]) if len(memo) > 0 else str(num),
+      numbers,
+      ""
+    )
+
+    print(equation)
+
