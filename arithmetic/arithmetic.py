c4882e84dae780f77a937a151df26ce730a08b7c
Quinton Harris (dznqbit) <quinton.harris@gmail.com>
2013-08-29 04:02:29 -0700

    Generating 4 random numbers

--- arithmetic/arithmetic.py
+++ arithmetic/arithmetic.py
@@ -1,7 +1,11 @@
+import random
+import string
+
 class app():
   def __init__(self):
     pass
 
   def run(self):
-    my_input = raw_input()
-    print("You wrote " + my_input);
+    bounds  = [int(x) for x in string.split(raw_input(), " ")]
+    numbers = [random.randrange(bounds[0], bounds[1]) for i in range(4)]
+    print(str(numbers).strip('[]'))
--- arithmetic/main.py
+++ arithmetic/main.py
@@ -1,3 +1,5 @@
+# http://www.reddit.com/r/dailyprogrammer/comments/1k7s7p/081313_challenge_135_easy_arithmetic_equations/
+
 import arithmetic
 
 arithmetic.app().run();
