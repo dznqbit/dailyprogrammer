ec9295ffd6501286b04afe97235055e2320a23e0
Quinton Harris (dznqbit) <quinton.harris@gmail.com>
2013-08-29 04:55:04 -0700

    Working arithmetic

--- arithmetic/arithmetic.py
+++ arithmetic/arithmetic.py
@@ -2,22 +2,39 @@ import random
 import string
 
 class app():
-  def __init__(self):
-    pass
-
   def run(self):
     bounds  = [int(x) for x in string.split(raw_input(), " ")]
-    self.question(bounds)
 
-  def question(self, bounds):
-    numbers = [random.randrange(bounds[0], bounds[1]) for i in range(4)]
+    while 1 == 1:
+      equation  = self.build_equation(bounds)
+      answer    = self.answer(equation)
+
+      if answer == "q":
+        break
+      else:
+        print("Correct!")
+
+  def build_equation(self, bounds):
+    numbers = [random.randrange(min(bounds), max(bounds)) for i in range(4)]
 
     equation = reduce(
       lambda memo, num: 
-        ' '.join([memo, random.choice(["+", "-", "x"]), str(num)]) if len(memo) > 0 else str(num),
+        ' '.join([memo, random.choice(["+", "-", "*"]), str(num)]) if len(memo) > 0 else str(num),
       numbers,
       ""
     )
 
-    print(equation)
+    return equation
 
+  def answer(self, equation):
+    solution = eval(equation)
+
+    print(equation)
+    user_input = raw_input().lower()
+
+    while user_input not in ["q", str(solution)]:
+      print("Incorrect...")
+      print(equation)
+      user_input = raw_input().lower()
+
+    return user_input
--- arithmetic/main.py
+++ arithmetic/main.py
@@ -1,5 +1,44 @@
 # http://www.reddit.com/r/dailyprogrammer/comments/1k7s7p/081313_challenge_135_easy_arithmetic_equations/
 
-import arithmetic
+import random
+import string
 
-arithmetic.app().run();
+class arithmetic():
+  def run(self):
+    bounds  = [int(x) for x in string.split(raw_input(), " ")]
+
+    while 1 == 1:
+      equation  = self.build_equation(bounds)
+      answer    = self.answer(equation)
+
+      if answer == "q":
+        break
+      else:
+        print("Correct!")
+
+  def build_equation(self, bounds):
+    numbers = [random.randrange(min(bounds), max(bounds)) for i in range(4)]
+
+    equation = reduce(
+      lambda memo, num: 
+        ' '.join([memo, random.choice(["+", "-", "*"]), str(num)]) if len(memo) > 0 else str(num),
+      numbers,
+      ""
+    )
+
+    return equation
+
+  def answer(self, equation):
+    solution = eval(equation)
+
+    print(equation)
+    user_input = raw_input().lower()
+
+    while user_input not in ["q", str(solution)]:
+      print("Incorrect...")
+      print(equation)
+      user_input = raw_input().lower()
+
+    return user_input
+
+arithmetic().run()
