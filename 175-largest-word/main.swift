445cbbfd86b1a921aca7d69dadf96b6b35bec755
Quinton Harris (dznqbit) <quinton.harris@gmail.com>
2014-08-13 18:37:37 -0700

    Reading input

--- 175-largest-word/main.swift
+++ 175-largest-word/main.swift
@@ -1,6 +1,17 @@
 #!/usr/bin/env xcrun swift -i
+
+import Foundation
  
-println("Process.arguments gave args:")
-for s in Process.arguments {
-  println(s)
+let standardInput = NSFileHandle.fileHandleWithStandardInput()
+let standardInputData = standardInput.availableData
+let allInput = NSString(data: standardInputData, encoding:NSUTF8StringEncoding)
+let allLines = allInput.componentsSeparatedByString("\n") 
+
+for line in allLines {
+  println(line);
 }
+
+// load words + alphabet
+// collect real words
+// order by length descending
+// take first where length same
