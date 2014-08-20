0170fffe7920b0b0578270387db4d5b8cba0a94a
Quinton Harris (dznqbit) <quinton.harris@gmail.com>
2014-08-19 18:16:34 -0700

    Unicode ^^

--- 175-largest-word/main.swift
+++ 175-largest-word/main.swift
@@ -8,9 +8,11 @@ let allInput = NSString(data: standardInputData, encoding:NSUTF8StringEncoding) 
 let allLines: [String] = allInput.componentsSeparatedByString("\n") 
 
 // load words + alphabet
-let allWords = allLines[0].componentsSeparatedByString(" ").filter({ word in !word.isEmpty })
+let allWords = allLines[0]
+  .componentsSeparatedByCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+  .filter({ word in !word.isEmpty })
 
-let alphabetStrings : [String] = allLines[1].componentsSeparatedByString(" ")
+let alphabetStrings : [String] = allLines[1].componentsSeparatedByCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
 let alphabet: [Character] = alphabetStrings.map({ (string: String) -> Character in string[string.startIndex] })
 
 // collect real words
@@ -29,12 +31,25 @@ func isRealWord(word: String, alphabet: Array<Character>) -> Bool {
 }
 
 // debug
-// for word in allWords { println("All: \"\(word)\"") }
+for word in allWords { println("All: \"\(word)\"") }
 
 let validWords = allWords.filter({ word -> Bool in isRealWord(String(word), alphabet) })
 
-// debug
-// for word in validWords { println("Valid: \(word)") }
+if (validWords.isEmpty) {
+  println("No Words Found")
+} else {
+  // debug
+  // for word in validWords { println("Valid: \(word)") }
 
-// order by length descending
-// take first where length same
+  // order by length descending
+  let orderedAndValidWords = validWords.sorted { countElements($0) > countElements($1) }
+
+  // debug
+  //for word in orderedAndValidWords { println("Ordered and Valid: \(word)") }
+
+  // take first where length same
+  let numberOfCharactersInFirstWord = countElements(orderedAndValidWords[0])
+  let longestWords = orderedAndValidWords.filter { countElements($0) == numberOfCharactersInFirstWord }
+
+  println(" ".join(longestWords))
+}
